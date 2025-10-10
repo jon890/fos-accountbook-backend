535e56b6d5f2d942040b336e40fa29a65ff9bf5c
jon890 <jon89071@gmail.com>
2025-10-11 00:43:03 +0900

    💚 Fix uuid migration issues

--- src/main/java/com/bifos/accountbook/domain/repository/UserRepository.java
+++ src/main/java/com/bifos/accountbook/domain/repository/UserRepository.java
@@ -5,14 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     
     Optional<User> findByEmail(String email);
     
-    Optional<User> findByUuid(UUID uuid);
+    Optional<User> findByUuid(String uuid); // VARCHAR(36) 문자열 형식
     
     boolean existsByEmail(String email);
 }

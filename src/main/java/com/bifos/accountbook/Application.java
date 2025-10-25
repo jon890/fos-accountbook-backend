74c1e76fa493009f3a9b2d4e09d7735d5ae48c08
jon890 <jon89071@gmail.com>
2025-10-25 22:39:32 +0900

    refactor: import 문 개선 및 불필요한 의존성 제거
    - 풀패키지 경로 대신 import 문 사용으로 코드 가독성 향상
    - UserDetailsServiceAutoConfiguration import 추가
    - 사용하지 않는 Environment 의존성 제거
    - 불필요한 @RequiredArgsConstructor 어노테이션 제거

--- src/main/java/com/bifos/accountbook/Application.java
+++ src/main/java/com/bifos/accountbook/Application.java
@@ -1,24 +1,17 @@
 package com.bifos.accountbook;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Slf4j
-@SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration.class
-})
+@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
 @EnableJpaAuditing
 @ConfigurationPropertiesScan
-@RequiredArgsConstructor
 public class Application {
-
-    private final Environment env;
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }

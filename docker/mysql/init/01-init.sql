79ea84ce79156155c5fe8bb9df4cc594c32d8499
jon890 <jon89071@gmail.com>
2025-11-18 10:42:08 +0900

    fix: MySQL 초기화 스크립트에서 타임존 설정 제거 (UTC 사용)

--- docker/mysql/init/01-init.sql
+++ docker/mysql/init/01-init.sql
@@ -8,14 +8,12 @@ SET CHARACTER SET utf8mb4;
 -- 데이터베이스가 이미 생성되어 있으므로 USE만 수행
 USE accountbook;
 
--- 타임존 설정
-SET time_zone = '+09:00';
+-- 타임존 설정 제거 (UTC 사용, Railway와 일치)
 
 -- 기본 설정 출력 (로그 확인용)
 SELECT 
     @@character_set_database as 'Database Charset',
-    @@collation_database as 'Database Collation',
-    @@time_zone as 'Timezone';
+    @@collation_database as 'Database Collation';
 
 -- 테이블 생성은 Spring Boot JPA가 자동으로 수행하거나
 -- Prisma 마이그레이션을 MySQL로 변환하여 사용할 수 있습니다

fc1b59c6b4915791b0b8b2525beb5578f90fabce
jon890 <jon89071@gmail.com>
2025-11-27 15:26:22 +0900

    ✅ 테스트 인코딩 문제 fix

--- src/test/java/com/bifos/accountbook/presentation/controller/AbstractControllerTest.java
+++ src/test/java/com/bifos/accountbook/presentation/controller/AbstractControllerTest.java
@@ -9,10 +9,12 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 /**
  * Presentation Layer 통합 테스트를 위한 추상 클래스
@@ -78,7 +80,10 @@ public abstract class AbstractControllerTest extends TestFixturesSupport {
 
   @BeforeEach
   void setup() {
-    mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                             .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                             .apply(springSecurity())
+                             .build();
     objectMapper = new ObjectMapper()
         .registerModule(new JavaTimeModule())
         .registerModule(new ParameterNamesModule());
